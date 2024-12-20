a12ab409deda0a42ad100780f5bf3127e68c704a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-12-19 20:48:23 -0800

    Update Begging.js

--- Begging.js
+++ Begging.js
@@ -35,3 +35,12 @@ function beggars(values, n){
 }
 
 // or
+
+function beggars(values, n) {
+  let take = Array(n).fill(0);
+  
+  for (let i = 0; i < values.length; i++)
+    take[i % n] += values[i];
+    
+  return take;
+}
